332269c56d548236d86fd19a1ea31875317eae75
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-15 18:06:03 -0500

    some changes

--- routes/cart.js
+++ routes/cart.js
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
     const menuItems = await userQueries.queryAllFoodItems();
 
     // Pass user object to the menu template
-    res.render('menu', { user, menuItems: menuItems.rows });
+    res.render('cart', { user, menuItems: menuItems.rows });
   } catch (error) {
     console.error("Error fetching user and menu items:", error);
     res.status(500).send("Internal Server Error");
