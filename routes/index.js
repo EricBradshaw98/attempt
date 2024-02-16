72170742622f46d9b6d61939ed658c11b5b58d39
Eric <ericbradshaw98@gmail.com>
2024-02-15 23:42:43 -0500

    added what we could

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,22 @@
-"use strict";
-
-// Basic express setup:
-
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
-
-// Update the dates for the initial tweets (data-files/initial-tweets.json).
-require("./lib/date-adjust")();
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+const express = require('express');
+const router  = express.Router();
+const userQueries = require('../db/queries/users');
 
 
 
+
+router.get("/", (req, res) => {
+
+  req.session = null;
+
+  res.redirect(`/login`);
+});
+
+router.post("/", (req, res) => {
+
+  req.session = null;
+
+  res.redirect(`/login`);
+});
+//==============================================================================
+module.exports = router;
